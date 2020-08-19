19a6ddf9ed5053e5be6d3f576536c6e25e3ca911
Aviral Singh Chauhan <59026666+sAVItar02@users.noreply.github.com>
2020-08-19 17:10:21 +0530

    Initial commit

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    [SerializeField] int loadDelay = 4;
+    int currentSceneIndex;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        StartCoroutine(WaitForTime());
+    }
+
+    IEnumerator WaitForTime()
+    {
+        yield return new WaitForSeconds(loadDelay);
+        LoadNextScene();
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Glitch_Garden
+ 
