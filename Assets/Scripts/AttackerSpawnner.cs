314c6bd0df434a53e8b6c883dc68d8c6fe25368c
Aviral Singh Chauhan <59026666+sAVItar02@users.noreply.github.com>
2020-09-05 02:49:55 +0530

    Attacker animation and movement setup

---
+++ Assets/Scripts/AttackerSpawnner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackerSpawnner : MonoBehaviour
+{
+    [SerializeField] float minSpawnDelay = 1f;
+    [SerializeField] float maxSpawnDelay = 5f;
+    [SerializeField] Attacker attackerPrefab;
+    [SerializeField] bool spawn = true;
+
+    IEnumerator Start()
+    {
+        while(spawn)
+        {
+            yield return new WaitForSeconds(Random.Range(minSpawnDelay, maxSpawnDelay));
+            SpawnEnemy();
+        }
+    }
+
+    private void SpawnEnemy()
+    {
+        Instantiate(attackerPrefab, transform.position, transform.rotation);
+    }
+}
