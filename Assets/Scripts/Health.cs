1703cadba79c14d7bcea817449cfe7124d27f41a
Aviral Singh Chauhan <59026666+sAVItar02@users.noreply.github.com>
2020-09-06 18:04:50 +0530

    Enemy Health and damage script (buggy)

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] float health = 100f;
+    [SerializeField] ParticleSystem deatVFX;
+    public void DealDamage(float damage)
+    {
+        health -= damage;
+        if(health <= 0)
+        {
+            GetComponent<Attacker>().SetMoveSpeed(0f);
+            Destroy(gameObject, 0.2f);
+            GetComponent<Animator>().enabled = false;
+            var VFX = Instantiate(deatVFX, transform.position, Quaternion.identity);
+            Destroy(VFX, 1f);
+        }
+    }
+}
--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -5,12 +5,19 @@ using UnityEngine;
 public class Projectile : MonoBehaviour
 {
     [SerializeField] float projectileSpeed = 1f;
-    void Start()
-    {
-        
-    }
+    [SerializeField] float damage = 50f;
     void Update()
     {
         transform.Translate(Vector2.right * projectileSpeed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D otherCollider)
+    {
+        if(otherCollider.tag == "Attacker")
+        {
+            var health = otherCollider.GetComponent<Health>();
+            health.DealDamage(damage);
+            Destroy(gameObject);
+        }
     }
 }
